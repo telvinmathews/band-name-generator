d4a56a1740157bb9a9f0f4f47e21a985eeb4cd19
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-12-11 00:09:38 -0600

    put all code into its own fuction to clean up

--- main.py
+++ main.py
@@ -16,15 +16,20 @@ def band_name_gen():
     print("Your band name is: " + user_city + " " + user_pet)
 
 # print("Try another name?")
-choice_one = "Y"
-choice_two = "N"
-user_choice = input("Try another name (Y/N)?\n")
+def user_selection():
+    choice_one = "Y"
+    choice_two = "N"
+    user_choice = input("Try another name (Y/N)?\n")
+    if user_choice == choice_one:
+        band_name_gen()
+    else:
+        print("Goodbye!")
 
 # user_choice = input("Y/N")
 # if user type "Y"
-if user_choice == choice_one:
-    band_name_gen()
-else: print("Goodbye!")
+band_name_gen()
+user_selection()
+
 
 
 # repeat program
