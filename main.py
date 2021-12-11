3a8a607afeda4ed4476764932d9397d57cb13674
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-12-10 23:59:48 -0600

    added fuction

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-#1. Create a greeting for your program.
+def band_name_gen():
+    #1. Create a greeting for your program.
 
-print("Welcome to the band name Generator! ")
+    print("Welcome to the band name Generator! ")
 
-#2. Ask the user for the city that they grew up in.
+    #2. Ask the user for the city that they grew up in.
 
-user_city = input("What city did you grow up in?\n")
+    user_city = input("What city did you grow up in?\n")
 
-#3. Ask the user for the name of a pet.
+    #3. Ask the user for the name of a pet.
 
-user_pet = input("What is/was the name of your favorite pet?\n")
+    user_pet = input("What is/was the name of your favorite pet?\n")
 
-#4. Combine the name of their city and pet and show them their band name.
+    #4. Combine the name of their city and pet and show them their band name.
 
-print("Your band name is: " + user_city + " " + user_pet)
+    print("Your band name is: " + user_city + " " + user_pet)
 
 # print("Try another name?")
+choice_one = "Y"
+choice_two = "N"
 user_choice = input("Try another name (Y/N)?\n")
+
 # user_choice = input("Y/N")
 # if user type "Y"
+if user_choice == choice_one:
+    band_name_gen()
+else: print("Goodbye!")
+
+
 # repeat program
+
 # if user type "N"
 # print("Goodbye!")
